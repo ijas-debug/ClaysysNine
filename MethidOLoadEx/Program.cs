281eae7e6ab7ce5648169478cd330301ba0295c7
Ijas <118385369+ijas-debug@users.noreply.github.com>
2023-06-17 21:51:02 +0530

    Add files via upload

---
+++ MethidOLoadEx/Program.cs
@@ -0,0 +1,45 @@
+﻿
+
+using System;
+
+namespace ConstructorEx
+{
+    public class Fruit
+    {
+        public string color;
+        public string name;
+        public int quantity;
+        public int price;
+
+        public Fruit(string fruitColor, string fruitName) //constructor
+        {
+            color = fruitColor;
+            name = fruitName;
+        }
+        public Fruit(int fruitquantity,int fruitprice) 
+        {
+            quantity = fruitquantity;
+            price= fruitprice;
+        }
+        public void DisplayName()
+        {
+            Console.WriteLine(name);
+        }
+        public void DisplayPrice()
+        {
+            Console.WriteLine(price);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fruit apple = new Fruit("Red", "Apple");
+            apple.DisplayName();
+            Fruit apple = new Fruit("5", "50");
+            apple.DisplayPrice();
+
+        }
+    }
+}
---
+++ StringBuilderex/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using System.Text;
+
+
+
+    StringBuilder sb=new StringBuilder("Random Text");
+    StringBuilder sb2 = new StringBuilder("More Stuff that is very Important",256);
+    Console.WriteLine("Capacity :{0}, sb2.Capacity");
+    Console.WriteLine("Length :{0}, sb2.Length");
+    sb2.AppendLine("\nMore important text");
+    CultureInfo enUs = CultureInfo.CreateSpecificCulture("en-US");
+    string bestCust = "Bob Smith";
+    sb2.AppendFormat(enUs, "Best Customer : {0}", bestCust);
+    Console.WriteLine(sb2.ToString());
+    sb2.Replace("text", "characters");
+    Console.WriteLine(sb2.ToString());
+    sb2.Clear();
+    sb2.Append("Random Text");
+    Console.WriteLine(sb.Equals(sb2));
+    sb2.Insert(11, "that's great");
+    Console.WriteLine(sb2.ToString());
+    sb2.Remove(11, 7);
+    Console.WriteLine(sb2.ToString());
